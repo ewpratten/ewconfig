abd2be6aec0a462abda62e30c4538990c24d3aaf
Evan Pratten <ewpratten@gmail.com>
2023-03-08 12:18:05 -0500

    cli macro for sheridan rdp

--- configs/zsh/macros.sh
+++ configs/zsh/macros.sh
@@ -10,6 +10,7 @@ alias nvim-tmp="nvim $(mktemp)"
 alias flush-dns="sudo systemd-resolve --flush-caches"
 alias showsizes="du -h --max-depth=1"
 alias lsgrep="ls | grep"
+alias sheridan-rdp='firefox --new-window "ext+container:name=College&url=https://client.wvd.microsoft.com/arm/webclient/index.html"'
 
 # WHOIS macros
 alias whois-afrinic="whois -h whois.afrinic.net"
