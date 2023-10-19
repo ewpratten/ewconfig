bfcc543e6586e8c82fc11c9cf4888605ed32cb97
Evan Pratten <evan@ewpratten.com>
2023-10-19 11:20:21 -0400

    fix reload commands

--- configs/shells/zsh/macros.sh
+++ configs/shells/zsh/macros.sh
@@ -1,6 +1,9 @@
 # We are compatible with bash, so first load the bash-specific macros
 . $EWCONFIG_ROOT/configs/shells/bash/macros.sh
 
+# Aliases
+alias zshreload="source ~/.zshrc"
+
 # Search for a process
 proc-grep() {
     if [ $# != 1 ]; then
