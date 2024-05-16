6bd3f2c3254aa667efd65ce45a1e5f0ca92cd188
Evan Pratten <evan@ewpratten.com>
2024-05-16 10:25:16 -0400

    Clean up houdini scripts

--- python_modules/ewpipe/common/logging.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-
-def configure_logging(verbose: bool = False):
-    logging.basicConfig(
-        level=logging.DEBUG if verbose else logging.INFO,
-        format="%(levelname)s:\t%(message)s",
-    )
